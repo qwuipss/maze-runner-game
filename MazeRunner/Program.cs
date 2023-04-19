01b378fafcd3654789765db3846958ed3000cf7c
awen <quetent@icloud.com>
2023-04-19 13:24:45 +0500

    initial commit

---
+++ MazeRunner/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MazeRunner;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        using var game = new MazeRunner.MazeRunnerGame();
+        game.Run();
+    }
+}
