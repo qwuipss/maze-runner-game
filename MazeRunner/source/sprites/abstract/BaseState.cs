ae93463101c15f44e7226b206174dc9e7bf96103
awen <quetent@icloud.com>
2023-05-01 01:51:47 +0500

    add base state class

---
+++ MazeRunner/source/sprites/abstract/BaseState.cs
@@ -0,0 +1,54 @@
+﻿#region Usings
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace MazeRunner.Sprites.States;
+
+public abstract class BaseState : ISpriteState
+{
+    public abstract Texture2D Texture { get; }
+
+    public abstract int FramesCount { get; }
+
+    public virtual int Width
+    {
+        get
+        {
+            return Texture.Width / FramesCount;
+        }
+    }
+
+    public virtual int Height
+    {
+        get
+        {
+            return Texture.Height;
+        }
+    }
+
+    public virtual int AnimationDelayMs
+    {
+        get
+        {
+            return 50;
+        }
+    }
+
+    public Point CurrentAnimationPoint
+    {
+        get
+        {
+            return new Point(CurrentAnimationPointX, 0);
+        }
+    }
+
+    public virtual ISpriteState ProcessState()
+    {
+        CurrentAnimationPointX = (CurrentAnimationPointX + Width) % (Width * FramesCount);
+
+        return this;
+    }
+
+    protected virtual int CurrentAnimationPointX { get; set; }
+}
--- MazeRunner/source/sprites/hero/states/HeroIdleState.cs
+++ MazeRunner/source/sprites/hero/states/HeroIdleState.cs
@@ -1,16 +1,13 @@
 ﻿#region Usings
 using MazeRunner.Content;
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 #endregion
 
 namespace MazeRunner.Sprites.States;
 
-public class HeroIdleState : ISpriteState
+public class HeroIdleState : BaseState
 {
-    private int _currentAnimationPointX;
-
-    public Texture2D Texture
+    public override Texture2D Texture
     {
         get
         {
@@ -18,50 +15,11 @@ public class HeroIdleState : ISpriteState
         }
     }
 
-    public int FramesCount
+    public override int FramesCount
     {
         get
         {
             return 6;
         }
-    }
-
-    public int Width
-    {
-        get
-        {
-            return Texture.Width / FramesCount;
-        }
-    }
-
-    public int Height
-    {
-        get
-        {
-            return Texture.Height;
-        }
-    }
-
-    public int AnimationDelayMs
-    {
-        get
-        {
-            return 50;
-        }
-    }
-
-    public Point CurrentAnimationPoint
-    {
-        get
-        {
-            return new Point(_currentAnimationPointX, 0);
-        }
-    }
-
-    public ISpriteState ProcessState()
-    {
-        _currentAnimationPointX = (_currentAnimationPointX + Width) % (Width * FramesCount);
-
-        return this;
     }
 }
