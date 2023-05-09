7b76d72a48d76c09ec62edd122357ce59e2fa356
awen <quetent@icloud.com>
2023-05-09 15:48:19 +0500

    roll chance remove

--- MazeRunner/source/helpers/RandomHelper.cs
+++ MazeRunner/source/helpers/RandomHelper.cs
@@ -6,11 +6,6 @@ public static class RandomHelper
 {
     private static readonly Random _random = new();
 
-    public static bool RollChance(double chance)
-    {
-        return chance > _random.NextDouble();
-    }
-
     public static int Next(int minValue, int maxValue)
     {
         return _random.Next(minValue, maxValue);
